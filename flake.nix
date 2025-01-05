324c53881eb21de4331a899f0724a6ea06c5efab
Baitinq <manuelpalenzuelamerino@gmail.com>
2025-01-05 15:32:57 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Renfe flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    zig.url = "github:mitchellh/zig-overlay";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    zig,
+  }: let
+    systems = ["x86_64-darwin" "aarch64-darwin" "x86_64-linux"];
+    createDevShell = system: let
+      pkgs = import nixpkgs {
+        system = "${system}";
+        config.allowUnfree = true;
+      };
+    in
+      pkgs.mkShell {
+        buildInputs = with pkgs; [
+            zig.packages."${system}".master
+        ];
+      };
+  in {
+    devShell = nixpkgs.lib.genAttrs systems createDevShell;
+  };
+}
