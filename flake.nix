162951e04bbb79fd51fe2ebc0bc2f7c2b242a2ea
Baitinq <manuelpalenzuelamerino@gmail.com>
2025-05-13 22:26:47 +0200

    Misc: Cleanup flake.nix

--- flake.nix
+++ flake.nix
@@ -1,38 +1,37 @@
 {
-  description = "Renfe flake";
+  description = "Interpreter flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    zig.url = "github:mitchellh/zig-overlay";
+    zig.url    = "github:mitchellh/zig-overlay";
     zls = {
       url = "github:zigtools/zls";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    zig,
-    zls,
-  }: let
+  outputs = { self, nixpkgs, zig, zls }: let
     systems = ["x86_64-darwin" "aarch64-darwin" "x86_64-linux"];
+
     createDevShell = system: let
       pkgs = import nixpkgs {
-        system = "${system}";
+        inherit system;
         config.allowUnfree = true;
       };
-    in
-      pkgs.mkShell {
-        buildInputs = with pkgs; [
-            zig.packages."${system}".master
-            zls.packages."${system}".default
-            llvmPackages_20.libllvm
-            gdb
-            gf
-            valgrind
-        ];
-      };
+      isDarwin = pkgs.stdenv.isDarwin;
+    in pkgs.mkShell {
+      buildInputs = with pkgs; [
+        zig.packages."${system}".master
+        zls.packages."${system}".default
+      ]
+      ++ pkgs.lib.optional isDarwin llvmPackages_19.libllvm
+      ++ pkgs.lib.optionals (!isDarwin) ([
+        llvmPackages_20.libllvm
+        gdb
+        gf
+        valgrind
+      ]);
+    };
   in {
     devShell = nixpkgs.lib.genAttrs systems createDevShell;
   };
