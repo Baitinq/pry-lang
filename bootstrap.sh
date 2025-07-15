fdfa94b29abac94107347be579a452184ba4f415
Baitinq <manuelpalenzuelamerino@gmail.com>
2025-07-15 16:40:37 +0200

    Boostrap: Improve boostrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-zig build run -- src/bootstrap/main.pry && cc $(llvm-config --libs) output.o -o stage1 && ./stage1 src/bootstrap/main.pry && cc $(llvm-config --libs) bootstrap_output.o -o stage2 && ./stage2 src/bootstrap/main.pry && cc $(llvm-config --libs) bootstrap_output.o -o stage3
+echo "Cleaning up previous builds..."
+rm -f stage1 stage2 stage3 output.o bootstrap_output.o
+
+echo "Building stage 0..."
+
+zig build
+
+echo "Building stage 1..."
+
+./zig-out/bin/pry-lang src/bootstrap/main.pry && cc $(llvm-config --libs) output.o -o stage1
+
+echo "Building stage 2..."
+
+./stage1 src/bootstrap/main.pry && cc $(llvm-config --libs) bootstrap_output.o -o stage2
+
+echo "Building stage 3..."
+
+./stage2 src/bootstrap/main.pry && cc $(llvm-config --libs) bootstrap_output.o -o stage3
